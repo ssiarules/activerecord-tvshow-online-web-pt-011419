a7ed625b209a354fada6e9a0d3e91f4fdab7bc8c
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-12 13:42:58 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,23 @@
+class Show < ActiveRecord::Base
+  
+ def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where("rating = ?", self.highest_rating).first
+  end
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end 
+  
+  def self.least_popular_show
+    Show.where("rating = ?", self.lowest_rating).first
+  end 
+  
+  def self.ratings_sum
+    Show.sum()
+  end 
+  
+end
